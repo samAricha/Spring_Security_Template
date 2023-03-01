566d303d96683f795804ef3a1c1c666a2ef96178
samAricha <samaricha001@gmail.com>
2023-03-02 01:06:00 +0300

    User login done

--- src/main/java/com/example/spring_security_demo/controllers/AuthController.java
+++ src/main/java/com/example/spring_security_demo/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.spring_security_demo.controllers;
 
+import com.example.spring_security_demo.dto.LoginDto;
 import com.example.spring_security_demo.dto.RegisterDto;
 import com.example.spring_security_demo.models.Roles;
 import com.example.spring_security_demo.models.UserEntity;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,6 +39,20 @@ public class AuthController {
         this.userRepository = userRepository;
         this.rolesRepository = rolesRepository;
         this.passwordEncoder = passwordEncoder;
+
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto){
+
+        Authentication authentication = authenticationManager
+                .authenticate(
+                        new UsernamePasswordAuthenticationToken(loginDto.getUsername(),
+                                loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return new ResponseEntity<>("User signed success!", HttpStatus.BAD_REQUEST);
 
     }
 
---
+++ src/main/java/com/example/spring_security_demo/dto/LoginDto.java
@@ -0,0 +1,10 @@
+package com.example.spring_security_demo.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String username;
+    private String password;
+
+}
